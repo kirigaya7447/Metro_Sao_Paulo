cc9444c21f47de53d463ac9930a7d45e96a47b3d
kirigaya7447 <joaoh2915@gmail.com>
2026-02-02 19:04:11 -0300

    Estações descritas no js, textos atualizados no html

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
-Lik: https://kirigaya7447.github.io/Metro_Sao_Paulo/
+Link: https://kirigaya7447.github.io/Metro_Sao_Paulo/
 
 Projeto de orientação sobre utilização do metrô de São Paulo, SP - Brasil.
---
+++ Style/fourHorseman.js
@@ -0,0 +1,231 @@
+/*Linhas
+Linha 1 - Azul
+    Jabaquara-Comitê Paralímpico Brasileiro
+    Conceição
+    São Judas
+    Saúde-Ultrafarma
+    Praça da Árvore
+    Santa Cruz
+    Vila Mariana
+    Ana Rosa
+    Paraíso
+    Vergueiro-Sebrae
+    São Joaquim
+    Japão-Liberdade
+    Sé
+    São Bento
+    Luz
+    Tiradentes
+    Armênia
+    Portuguesa-Tietê
+    Carandiru
+    Santana
+    Jardim São Paulo-Ayrton Senna
+    Parada Inglesa
+    Tucuruvi
+
+
+Linha 2 - Verde
+    Vila Prudente
+    Tamanduateí
+    Sacomã
+    Alto do Ipiranga
+    Santos-Imigrantes
+    Chácara Klabin
+    Ana Rosa
+    Paraíso
+    Brigadeiro
+    Trianon-Masp
+    Consolação
+    Clínicas
+    S. N. Sra. de Fátima-Sumaré
+    Vila Madalena
+
+Linha 3 - Vermelha
+    Corinthians-Itaquera
+    Artur Alvim
+    Patriarca-Vila Ré
+    Guilhermina-Esperança
+    Vila Matilde
+    Penha-Lojas Besni
+    Carrão-Assaí Atacadista
+    Tatuapé
+    Belém
+    Bresser-Mooca
+    Brás
+    Pedro II
+    Sé
+    Anhangabaú
+    República
+    Santa Cecília
+    Marechal Deodoro
+    Palmeiras-Barra Funda
+
+Linha 4 - Amarela
+    Luz
+    República
+    Higienópolis-Mackenzie
+    Paulista - Pernambucanas
+    Oscar Freire
+    Fradique Coutinho
+    Faria Lima
+    Pinheiros
+    Butantã
+    São Paulo-Morumbi
+    Vila Sônia - Professora Elizabeth Tenreiro
+
+Linha 5 - Lilás
+    Capão Redondo
+    Campo Limpo
+    Vila das Belezas
+    Giovanni Gronchi
+    Santo Amaro
+    Largo Treze
+    Adolfo Pinheiro
+    Alto da Boa Vista
+    Borba Gato
+    Brooklin
+    Campo Belo
+    Eucaliptos
+    Moema
+    AACD - Servidor
+    Hospital São Paulo
+    Santa Cruz
+    Chácara Klabin
+
+Linha 7 - Rubi
+    Jundiaí
+    Várzea Paulista
+    Campo Limpo Paulista
+    Botujuru
+    Francisco Morato
+    Baltazar Fidélis
+    Franco da Rocha
+    Caieiras
+    Perus
+    Vila Aurora
+    Jaraguá
+    Vila Clarice
+    Pirituba
+    Piqueri
+    Lapa
+    Água Branca
+    Palmeiras-Barra Funda
+
+Linha 8 - Diamante
+    Júlio Prestes
+    Palmeiras-Barra Funda
+    Lapa
+    Domingos De Moraes
+    Imperatriz Leopoldina
+    Presidente Altino
+    Osasco
+    Comandante Sampaio
+    Quitaúna
+    General Miguel Costa
+    Carapicuíba
+    Santa Terezinha
+    Antonio João
+    Barueri
+    Jardim Belval
+    Jardim Silveira
+    Jandira
+    Sagrado Coração
+    Engenheiro Cardoso
+    Itapevi
+    Santa Rita
+    Ambuitá
+    Amador Bueno
+
+Linha 9 - Esmeralda
+Osasco
+Presidente Altino
+Ceasa
+Villa Lobos-Jaguaré
+Cidade Universitária
+Pinheiros
+Hebraica-Rebouças
+Cidade Jardim
+Vila Olímpia
+Berrini
+Morumbi
+Granja Julieta
+João Dias
+Santo Amaro
+Socorro
+Jurubatuba
+Autódromo
+Primavera-Interlagos
+Grajaú
+Bruno Covas/Mendes-Vila Natal
+Estação Varginha
+
+Linha 10 - Turquesa
+Palmeiras-Barra Funda
+Luz
+Brás
+Juventus-Mooca
+Ipiranga
+Tamanduateí
+São Caetano do Sul-Prefeito Walter Braido
+Utinga
+Prefeito Saladino
+Prefeito Celso Daniel-Santo André
+Capuava
+Mauá
+Guapituba
+Ribeirão Pires-Antônio Bespalec
+Rio Grande Da Serra
+
+Linha 11 - Coral
+Estudantes
+Mogi Das Cruzes
+Braz Cubas
+Jundiapeba
+Suzano
+Calmon Viana
+Poá
+Ferraz De Vasconcelos
+Antonio Gianetti Neto
+Guaianases
+José Bonifácio
+Dom Bosco
+Corinthians-Itaquera
+Tatuapé
+Brás
+Luz
+Palmeiras-Barra Funda
+
+Linha 12 - Safira
+Brás
+Tatuapé
+Engenheiro Goulart
+USP Leste
+Comendador Ermelino
+São Miguel Paulista
+Jardim Helena-Vila Mara
+Itaim Paulista
+Jardim Romano
+Engenheiro Manoel Feio
+Itaquaquecetuba
+Aracaré
+Calmon Viana
+
+Linha 13 - Jade
+Engenheiro Goulart
+Guarulhos-Cecap
+Aeroporto-Guarulhos
+
+Linha 15 - Prata
+    Vila Prudente
+    Oratório
+    São Lucas
+    Camilo Haddad
+    Vila Tolstói
+    Vila União
+    Jardim Planalto 
+    Sapopemba
+    Fazenda da Juta
+    São Mateus
+    Jardim Colonial
+*/
