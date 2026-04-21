b322ca6d02ff0c7bc14dfb63a8a2958e3da8d048
kirigaya7447 <joaoh2915@gmail.com>
2026-04-20 23:45:33 -0300

    Ajustando o js com as linhas

--- Style/fourHorseman.js
+++ Style/fourHorseman.js
@@ -1,220 +1,282 @@
+var linhaAzul = [
+    "Azul - Jabaquara-Comitê Paralímpico Brasileiro",
+    "Azul - Conceição",
+    "Azul - São Judas",
+    "Azul - Saúde-Ultrafarma",
+    "Azul - Praça da Árvore",
+    "Azul - Santa Cruz",
+    "Azul - Vila Mariana",
+    "Azul - Ana Rosa",
+    "Azul - Paraíso",
+    "Azul - Vergueiro-Sebrae",
+    "Azul - São Joaquim",
+    "Azul - Japão-Liberdade",
+    "Azul - Sé",
+    "Azul - São Bento",
+    "Azul - Luz",
+    "Azul - Tiradentes",
+    "Azul - Armênia",
+    "Azul - Portuguesa-Tietê",
+    "Azul - Carandiru",
+    "Azul - Santana",
+    "Azul - Jardim São Paulo-Ayrton Senna",
+    "Azul - Parada Inglesa",
+    "Azul - Tucuruvi"
+];
+var linhaVerde = [
+    "Verde - Vila Prudente",
+    "Verde - Tamanduateí",
+    "Verde - Sacomã",
+    "Verde - Alto do Ipiranga",
+    "Verde - Santos-Imigrantes",
+    "Verde - Chácara Klabin",
+    "Verde - Ana Rosa",
+    "Verde - Paraíso",
+    "Verde - Brigadeiro",
+    "Verde - Trianon-Masp",
+    "Verde - Consolação",
+    "Verde - Clínicas",
+    "Verde - S. N. Sra. de Fátima-Sumaré",
+    "Verde - Vila Madalena"
+];
+var linhaVermelha = [
+    "Vermelha - Corinthians-Itaquera",
+    "Vermelha - Artur Alvim",
+    "Vermelha - Patriarca-Vila Ré",
+    "Vermelha - Guilhermina-Esperança",
+    "Vermelha - Vila Matilde",
+    "Vermelha - Penha-Lojas Besni",
+    "Vermelha - Carrão-Assaí Atacadista",
+    "Vermelha - Tatuapé",
+    "Vermelha - Belém",
+    "Vermelha - Bresser-Mooca",
+    "Vermelha - Brás",
+    "Vermelha - Pedro II",
+    "Vermelha - Sé",
+    "Vermelha - Anhangabaú",
+    "Vermelha - República",
+    "Vermelha - Santa Cecília",
+    "Vermelha - Marechal Deodoro",
+    "Vermelha - Palmeiras-Barra Funda"
+];
+var linhaAmarela = [
+    "Amarela - Luz",
+    "Amarela - República",
+    "Amarela - Higienópolis-Mackenzie",
+    "Amarela - Paulista - Pernambucanas",
+    "Amarela - Oscar Freire",
+    "Amarela - Fradique Coutinho",
+    "Amarela - Faria Lima",
+    "Amarela - Pinheiros",
+    "Amarela - Butantã",
+    "Amarela - São Paulo-Morumbi",
+    "Amarela - Vila Sônia - Professora Elizabeth Tenreiro"
+];
+var linhaLilas = [
+    "Lilás - Capão Redondo",
+    "Lilás - Campo Limpo",
+    "Lilás - Vila das Belezas",
+    "Lilás - Giovanni Gronchi",
+    "Lilás - Santo Amaro",
+    "Lilás - Largo Treze",
+    "Lilás - Adolfo Pinheiro",
+    "Lilás - Alto da Boa Vista",
+    "Lilás - Borba Gato",
+    "Lilás - Brooklin",
+    "Lilás - Campo Belo",
+    "Lilás - Eucaliptos",
+    "Lilás - Moema",
+    "Lilás - AACD - Servidor",
+    "Lilás - Hospital São Paulo",
+    "Lilás - Santa Cruz",
+    "Lilás - Chácara Klabin"
+];
+var linhaRubi = [
+    "Rubi - Jundiaí",
+    "Rubi - Várzea Paulista",
+    "Rubi - Campo Limpo Paulista",
+    "Rubi - Botujuru",
+    "Rubi - Francisco Morato",
+    "Rubi - Baltazar Fidélis",
+    "Rubi - Franco da Rocha",
+    "Rubi - Caieiras",
+    "Rubi - Perus",
+    "Rubi - Vila Aurora",
+    "Rubi - Jaraguá",
+    "Rubi - Vila Clarice",
+    "Rubi - Pirituba",
+    "Rubi - Piqueri",
+    "Rubi - Lapa",
+    "Rubi - Água Branca",
+    "Rubi - Palmeiras-Barra Funda"
+];
+var linhaDiamante = [
+    "Diamante - Júlio Prestes",
+    "Diamante - Palmeiras-Barra Funda",
+    "Diamante - Lapa",
+    "Diamante - Domingos De Moraes",
+    "Diamante - Imperatriz Leopoldina",
+    "Diamante - Presidente Altino",
+    "Diamante - Osasco",
+    "Diamante - Comandante Sampaio",
+    "Diamante - Quitaúna",
+    "Diamante - General Miguel Costa",
+    "Diamante - Carapicuíba",
+    "Diamante - Santa Terezinha",
+    "Diamante - Antonio João",
+    "Diamante - Barueri",
+    "Diamante - Jardim Belval",
+    "Diamante - Jardim Silveira",
+    "Diamante - Jandira",
+    "Diamante - Sagrado Coração",
+    "Diamante - Engenheiro Cardoso",
+    "Diamante - Itapevi",
+    "Diamante - Santa Rita",
+    "Diamante - Ambuitá",
+    "Diamante - Amador Bueno"
+];
+var linhaEsmeralda = [
+    "Esmeralda - Osasco",
+    "Esmeralda - Presidente Altino",
+    "Esmeralda - Ceasa",
+    "Esmeralda - Villa Lobos-Jaguaré",
+    "Esmeralda - Cidade Universitária",
+    "Esmeralda - Pinheiros",
+    "Esmeralda - Hebraica-Rebouças",
+    "Esmeralda - Cidade Jardim",
+    "Esmeralda - Vila Olímpia",
+    "Esmeralda - Berrini",
+    "Esmeralda - Morumbi",
+    "Esmeralda - Granja Julieta",
+    "Esmeralda - João Dias",
+    "Esmeralda - Santo Amaro",
+    "Esmeralda - Socorro",
+    "Esmeralda - Jurubatuba",
+    "Esmeralda - Autódromo",
+    "Esmeralda - Primavera-Interlagos",
+    "Esmeralda - Grajaú",
+    "Esmeralda - Bruno Covas/Mendes-Vila Natal",
+    "Esmeralda - Estação Varginha"
+];
+var linhaTurquesa = [
+    "Turquesa - Palmeiras-Barra Funda",
+    "Turquesa - Luz",
+    "Turquesa - Brás",
+    "Turquesa - Juventus-Mooca",
+    "Turquesa - Ipiranga",
+    "Turquesa - Tamanduateí",
+    "Turquesa - São Caetano do Sul-Prefeito Walter Braido",
+    "Turquesa - Utinga",
+    "Turquesa - Prefeito Saladino",
+    "Turquesa - Prefeito Celso Daniel-Santo André",
+    "Turquesa - Capuava",
+    "Turquesa - Mauá",
+    "Turquesa - Guapituba",
+    "Turquesa - Ribeirão Pires-Antônio Bespalec",
+    "Turquesa - Rio Grande Da Serra"
+];
+var linhaCoral = [
+    "Coral - Estudantes",
+    "Coral - Mogi Das Cruzes",
+    "Coral - Braz Cubas",
+    "Coral - Jundiapeba",
+    "Coral - Suzano",
+    "Coral - Calmon Viana",
+    "Coral - Poá",
+    "Coral - Ferraz De Vasconcelos",
+    "Coral - Antonio Gianetti Neto",
+    "Coral - Guaianases",
+    "Coral - José Bonifácio",
+    "Coral - Dom Bosco",
+    "Coral - Corinthians-Itaquera",
+    "Coral - Tatuapé",
+    "Coral - Brás",
+    "Coral - Luz",
+    "Coral - Palmeiras-Barra Funda"
+];
+var linhaSafira = [
+    "Safira - Brás",
+    "Safira - Tatuapé",
+    "Safira - Engenheiro Goulart",
+    "Safira - USP Leste",
+    "Safira - Comendador Ermelino",
+    "Safira - São Miguel Paulista",
+    "Safira - Jardim Helena-Vila Mara",
+    "Safira - Itaim Paulista",
+    "Safira - Jardim Romano",
+    "Safira - Engenheiro Manoel Feio",
+    "Safira - Itaquaquecetuba",
+    "Safira - Aracaré",
+    "Safira - Calmon Viana"
+];
+var linhaJade = [
+    "Jade - Engenheiro Goulart",
+    "Jade - Guarulhos-Cecap",
+    "Jade - Aeroporto-Guarulhos"
+];
+var linhaPrata = [
+    "Prata - Vila Prudente",
+    "Prata - Oratório",
+    "Prata - São Lucas",
+    "Prata - Camilo Haddad",
+    "Prata - Vila Tolstói",
+    "Prata - Vila União",
+    "Prata - Jardim Planalto",
+    "Prata - Sapopemba",
+    "Prata - Fazenda da Juta",
+    "Prata - São Mateus",
+    "Prata - Jardim Colonial"
+];
+
 /*Linhas
-Linha 1 - Azul
-    Azul - Jabaquara-Comitê Paralímpico Brasileiro
-    Azul - Conceição
-    Azul - São Judas
-    Azul - Saúde-Ultrafarma
-    Azul - Praça da Árvore
-    Azul - Santa Cruz
-    Azul - Vila Mariana
-    Azul - Ana Rosa
-    Azul - Paraíso
-    Azul - Vergueiro-Sebrae
-    Azul - São Joaquim
-    Azul - Japão-Liberdade
-    Azul - Sé
-    Azul - São Bento
-    Azul - Luz
-    Azul - Tiradentes
-    Azul - Armênia
-    Azul - Portuguesa-Tietê
-    Azul - Carandiru
-    Azul - Santana
-    Azul - Jardim São Paulo-Ayrton Senna
-    Azul - Parada Inglesa
-    Azul - Tucuruvi
 
 
 Linha 2 - Verde
-    Verde - Vila Prudente
-    Verde - Tamanduateí
-    Verde - Sacomã
-    Verde - Alto do Ipiranga
-    Verde - Santos-Imigrantes
-    Verde - Chácara Klabin
-    Verde - Ana Rosa
-    Verde - Paraíso
-    Verde - Brigadeiro
-    Verde - Trianon-Masp
-    Verde - Consolação
-    Verde - Clínicas
-    Verde - S. N. Sra. de Fátima-Sumaré
-    Verde - Vila Madalena
+    
 
 Linha 3 - Vermelha
-    Vermelha - Corinthians-Itaquera
-    Vermelha - Artur Alvim
-    Vermelha - Patriarca-Vila Ré
-    Vermelha - Guilhermina-Esperança
-    Vermelha - Vila Matilde
-    Vermelha - Penha-Lojas Besni
-    Vermelha - Carrão-Assaí Atacadista
-    Vermelha - Tatuapé
-    Vermelha - Belém
-    Vermelha - Bresser-Mooca
-    Vermelha - Brás
-    Vermelha - Pedro II
-    Vermelha - Sé
-    Vermelha - Anhangabaú
-    Vermelha - República
-    Vermelha - Santa Cecília
-    Vermelha - Marechal Deodoro
-    Vermelha - Palmeiras-Barra Funda
+    
 
 Linha 4 - Amarela
-    Amarela - Luz
-    Amarela - República
-    Amarela - Higienópolis-Mackenzie
-    Amarela - Paulista - Pernambucanas
-    Amarela - Oscar Freire
-    Amarela - Fradique Coutinho
-    Amarela - Faria Lima
-    Amarela - Pinheiros
-    Amarela - Butantã
-    Amarela - São Paulo-Morumbi
-    Amarela - Vila Sônia - Professora Elizabeth Tenreiro
+    
 
 Linha 5 - Lilás
-    Lilás - Capão Redondo
-    Lilás - Campo Limpo
-    Lilás - Vila das Belezas
-    Lilás - Giovanni Gronchi
-    Lilás - Santo Amaro
-    Lilás - Largo Treze
-    Lilás - Adolfo Pinheiro
-    Lilás - Alto da Boa Vista
-    Lilás - Borba Gato
-    Lilás - Brooklin
-    Lilás - Campo Belo
-    Lilás - Eucaliptos
-    Lilás - Moema
-    Lilás - AACD - Servidor
-    Lilás - Hospital São Paulo
-    Lilás - Santa Cruz
-    Lilás - Chácara Klabin
+    
 
 Linha 7 - Rubi
-    Rubi - Jundiaí
-    Rubi - Várzea Paulista
-    Rubi - Campo Limpo Paulista
-    Rubi - Botujuru
-    Rubi - Francisco Morato
-    Rubi - Baltazar Fidélis
-    Rubi - Franco da Rocha
-    Rubi - Caieiras
-    Rubi - Perus
-    Rubi - Vila Aurora
-    Rubi - Jaraguá
-    Rubi - Vila Clarice
-    Rubi - Pirituba
-    Rubi - Piqueri
-    Rubi - Lapa
-    Rubi - Água Branca
-    Rubi - Palmeiras-Barra Funda
+    
 
 Linha 8 - Diamante
-    Júlio Prestes
-    Palmeiras-Barra Funda
-    Lapa
-    Domingos De Moraes
-    Imperatriz Leopoldina
-    Presidente Altino
-    Osasco
-    Comandante Sampaio
-    Quitaúna
-    General Miguel Costa
-    Carapicuíba
-    Santa Terezinha
-    Antonio João
-    Barueri
-    Jardim Belval
-    Jardim Silveira
-    Jandira
-    Sagrado Coração
-    Engenheiro Cardoso
-    Itapevi
-    Santa Rita
-    Ambuitá
-    Amador Bueno
+    
 
 Linha 9 - Esmeralda
-Osasco
-Presidente Altino
-Ceasa
-Villa Lobos-Jaguaré
-Cidade Universitária
-Pinheiros
-Hebraica-Rebouças
-Cidade Jardim
-Vila Olímpia
-Berrini
-Morumbi
-Granja Julieta
-João Dias
-Santo Amaro
-Socorro
-Jurubatuba
-Autódromo
-Primavera-Interlagos
-Grajaú
-Bruno Covas/Mendes-Vila Natal
-Estação Varginha
+    
 
 Linha 10 - Turquesa
-Turquesa - Palmeiras-Barra Funda
-Turquesa - Luz
-Turquesa - Brás
-Turquesa - Juventus-Mooca
-Turquesa - Ipiranga
-Turquesa - Tamanduateí
-Turquesa - São Caetano do Sul-Prefeito Walter Braido
-Turquesa - Utinga
-Turquesa - Prefeito Saladino
-Turquesa - Prefeito Celso Daniel-Santo André
-Turquesa - Capuava
-Turquesa - Mauá
-Turquesa - Guapituba
-Turquesa - Ribeirão Pires-Antônio Bespalec
-Turquesa - Rio Grande Da Serra
+    
 
 Linha 11 - Coral
-Coral - Estudantes
-Coral - Mogi Das Cruzes
-Coral - Braz Cubas
-Coral - Jundiapeba
-Coral - Suzano
-Coral - Calmon Viana
-Coral - Poá
-Coral - Ferraz De Vasconcelos
-Coral - Antonio Gianetti Neto
-Coral - Guaianases
-Coral - José Bonifácio
-Coral - Dom Bosco
-Coral - Corinthians-Itaquera
-Coral - Tatuapé
-Coral - Brás
-Coral - Luz
-Coral - Palmeiras-Barra Funda
+    
 
 Linha 12 - Safira
-Safira - Brás
-Safira - Tatuapé
-Safira - Engenheiro Goulart
-Safira - USP Leste
-Safira - Comendador Ermelino
-Safira - São Miguel Paulista
-Safira - Jardim Helena-Vila Mara
-Safira - Itaim Paulista
-Safira - Jardim Romano
-Safira - Engenheiro Manoel Feio
-Safira - Itaquaquecetuba
-Safira - Aracaré
-Safira - Calmon Viana
+    Safira - Brás
+    Safira - Tatuapé
+    Safira - Engenheiro Goulart
+    Safira - USP Leste
+    Safira - Comendador Ermelino
+    Safira - São Miguel Paulista
+    Safira - Jardim Helena-Vila Mara
+    Safira - Itaim Paulista
+    Safira - Jardim Romano
+    Safira - Engenheiro Manoel Feio
+    Safira - Itaquaquecetuba
+    Safira - Aracaré
+    Safira - Calmon Viana
 
 Linha 13 - Jade
-Jade - Engenheiro Goulart
-Jade - Guarulhos-Cecap
-Jade - Aeroporto-Guarulhos
+    Jade - Engenheiro Goulart
+    Jade - Guarulhos-Cecap
+    Jade - Aeroporto-Guarulhos
 
 Linha 15 - Prata
     Prata - Vila Prudente
